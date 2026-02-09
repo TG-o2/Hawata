f1f40025cca381daaa68cccce029cc65d26fc784
Tasnim Guesmi <tasnimguesmi02@gmail.com>
2026-02-09 22:52:13 +0100

    Revamp UI with new icons and layout adjustments
    Updated UI elements and added new icons for various functionalities in the application window.
    
    note: all UI elements/ icons /pictures are found in the folder "icons"

--- appwindow.cpp
+++ appwindow.cpp
@@ -1,31 +1,482 @@
 #include "appwindow.h"
 #include "ui_appwindow.h"
+#include "mainwindow.h"
+
 #include <QPixmap>
 #include <QMessageBox>
 #include <QFile>
 #include <QTextStream>
 #include <QMessageBox>
+
+
 appwindow::appwindow(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::appwindow)
 {
     ui->setupUi(this);
 
-    //photo logo set up
-    QPixmap pix("logoQt.png");
-    QPixmap user_icon("user.png");
-    ui->logo->setPixmap(pix);
-    ui->logo->setScaledContents(true);
-    ui->icon->setPixmap(user_icon);
-    ui->icon->setScaledContents(true);
-    //web / pages titles
-    ui->WebBrowsing->setTabText(0, "Users Admission");
-    ui->WebBrowsing->setTabText(1, "Test1");
-    ui->WebBrowsing->setTabText(2, "Test2");
 
-    //diferent bodies titles
-    ui->ToolBox_pages->setItemText(0, "User Creation");
-    ui->ToolBox_pages->setItemText(1, "Display");
+    //photo logo set up
+
+    QPixmap pix("chart.png");
+
+    QPixmap Uicon("icons/add-user2.png");
+    QPixmap Dock("icons/dock.png");
+    QPixmap Boat("icons/boat.png");
+    QPixmap Prod("icons/prod.png");
+    QPixmap Email("icons/Email.png");
+    QPixmap Comp("icons/companies.png");
+    QPixmap Stats("icons/stats.png");
+
+    QPixmap user_icon("icons/add-user1.png");
+    QPixmap user_display("icons/dashboard.png");
+    QPixmap user_delete("icons/trash.png");
+
+    //pictures for product--------
+    QPixmap fish("icons/fishicon.png");
+    QPixmap pic_2("logoQT.png");
+
+
+
+    //product picture set up
+    ui->pic->setPixmap(fish);
+    ui->pic->setScaledContents(true);
+    ui->pic_2->setPixmap(pic_2);
+    ui->pic_2->setScaledContents(true);
+
+
+
+
+    //end of product picture code--------------
+
+    //chart for page 4 of window 1 (User management)
+
+    ui->chart->setPixmap(pix);
+    ui->chart->setScaledContents(true);
+    ///----add manage display
+
+    //1
+    ui->adduser->setPixmap(user_icon);
+    ui->adduser->setScaledContents(true);
+
+    ui->Usericon->setPixmap(Uicon);
+    ui->Usericon->setScaledContents(true);
+
+
+    ui->deluser->setPixmap(user_delete);
+    ui->deluser->setScaledContents(true);
+
+    ui->dispuser->setPixmap(user_display);
+    ui->dispuser->setScaledContents(true);
+
+    //2
+    ui->adduser_2->setPixmap(user_icon);
+    ui->adduser_2->setScaledContents(true);
+
+    ui->deluser_2->setPixmap(user_delete);
+    ui->deluser_2->setScaledContents(true);
+
+    ui->dispuser_2->setPixmap(user_display);
+    ui->dispuser_2->setScaledContents(true);
+
+    //3
+    ui->adduser_3->setPixmap(user_icon);
+    ui->adduser_3->setScaledContents(true);
+
+    ui->deluser_3->setPixmap(user_delete);
+    ui->deluser_3->setScaledContents(true);
+
+    ui->dispuser_3->setPixmap(user_display);
+    ui->dispuser_3->setScaledContents(true);
+
+    //dock page 4,5 & 6
+    ui->adduser_4->setPixmap(Dock);
+    ui->adduser_4->setScaledContents(true);
+
+    ui->deluser_4->setPixmap(user_delete);
+    ui->deluser_4->setScaledContents(true);
+
+    ui->dispuser_4->setPixmap(user_display);
+    ui->dispuser_4->setScaledContents(true);
+
+    ui->adduser_5->setPixmap(Dock);
+    ui->adduser_5->setScaledContents(true);
+
+    ui->deluser_5->setPixmap(user_delete);
+    ui->deluser_5->setScaledContents(true);
+
+    ui->dispuser_5->setPixmap(user_display);
+    ui->dispuser_5->setScaledContents(true);
+
+    ui->adduser_6->setPixmap(Dock);
+    ui->adduser_6->setScaledContents(true);
+
+    ui->deluser_6->setPixmap(user_delete);
+    ui->deluser_6->setScaledContents(true);
+
+    ui->dispuser_6->setPixmap(user_display);
+    ui->dispuser_6->setScaledContents(true);
+    //boat page 7,8 & 9
+    ui->adduser_7->setPixmap(Boat);
+    ui->adduser_7->setScaledContents(true);
+
+    ui->deluser_7->setPixmap(user_delete);
+    ui->deluser_7->setScaledContents(true);
+
+    ui->dispuser_7->setPixmap(user_display);
+    ui->dispuser_7->setScaledContents(true);
+
+    ui->adduser_8->setPixmap(Boat);
+    ui->adduser_8->setScaledContents(true);
+
+    ui->deluser_8->setPixmap(user_delete);
+    ui->deluser_8->setScaledContents(true);
+
+    ui->dispuser_8->setPixmap(user_display);
+    ui->dispuser_8->setScaledContents(true);
+
+    ui->adduser_9->setPixmap(Boat);
+    ui->adduser_9->setScaledContents(true);
+
+    ui->deluser_9->setPixmap(user_delete);
+    ui->deluser_9->setScaledContents(true);
+
+    ui->dispuser_9->setPixmap(user_display);
+    ui->dispuser_9->setScaledContents(true);
+    //PRODUCT page 10, 12 & 11
+    ui->adduser_10->setPixmap(Prod);
+    ui->adduser_10->setScaledContents(true);
+
+    ui->deluser_10->setPixmap(user_delete);
+    ui->deluser_10->setScaledContents(true);
+
+    ui->dispuser_10->setPixmap(user_display);
+    ui->dispuser_10->setScaledContents(true);
+
+    ui->adduser_11->setPixmap(Prod);
+    ui->adduser_11->setScaledContents(true);
+
+    ui->deluser_11->setPixmap(user_delete);
+    ui->deluser_11->setScaledContents(true);
+
+    ui->dispuser_11->setPixmap(user_display);
+    ui->dispuser_11->setScaledContents(true);
+
+    ui->adduser_12->setPixmap(Prod);
+    ui->adduser_12->setScaledContents(true);
+
+    ui->deluser_12->setPixmap(user_delete);
+    ui->deluser_12->setScaledContents(true);
+
+    ui->dispuser_12->setPixmap(user_display);
+    ui->dispuser_12->setScaledContents(true);
+    //COMPANIES page 10, 12 & 11
+    ui->adduser_13->setPixmap(Comp);
+    ui->adduser_13->setScaledContents(true);
+
+    ui->deluser_13->setPixmap(Stats);
+    ui->deluser_13->setScaledContents(true);
+
+    ui->dispuser_13->setPixmap(Email);
+    ui->dispuser_13->setScaledContents(true);
+
+    ui->adduser_14->setPixmap(Comp);
+    ui->adduser_14->setScaledContents(true);
+
+    ui->deluser_14->setPixmap(Stats);
+    ui->deluser_14->setScaledContents(true);
+
+    ui->dispuser_14->setPixmap(Email);
+    ui->dispuser_14->setScaledContents(true);
+
+    ui->adduser_15->setPixmap(Comp);
+    ui->adduser_15->setScaledContents(true);
+
+    ui->deluser_15->setPixmap(Stats);
+    ui->deluser_15->setScaledContents(true);
+
+    ui->dispuser_15->setPixmap(Email);
+    ui->dispuser_15->setScaledContents(true);
+
+
+
+    ///-----end
+
+    //frame
+    ui->BackFrame->lower();
+    ui->BackFrame->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_2->lower();
+    ui->BackFrame_3->lower();
+    ui->BackFrame_4->lower();
+    ui->BackFrame_5->lower();
+    ui->BackFrame_6->lower();
+    ui->BackFrame_7->lower();
+    ui->BackFrame_8->lower();
+    ui->BackFrame_9->lower();
+    ui->BackFrame_10->lower();
+    ui->BackFrame_11->lower();
+    ui->BackFrame_12->lower();
+    ui->BackFrame_13->lower();
+    ui->BackFrame_14->lower();
+    ui->BackFrame_15->lower();
+
+    ui->BackFrame_2->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_3->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_4->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_5->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_6->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_7->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_8->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_9->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_10->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_11->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_12->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_13->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_14->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+    ui->BackFrame_15->setStyleSheet(
+        "background: rgba(255, 255, 255, 0.3);"
+        "border: 2px solid #42a5f5;"
+        "border-radius: 8px;"
+        );
+
+    ///=============web / pages titles START================
+    ui->WebBrowsing->setTabText(0, "👨‍💻 Users");
+    ui->WebBrowsing->setTabText(1, "🛳️ Manage Dock");
+    ui->WebBrowsing->setTabText(2, "⛵ Boats Admission");
+    ui->WebBrowsing->setTabText(3, "🐟  Product Restock");
+    ui->WebBrowsing->setTabText(4, "🏢  Companies");
+    ui->WebBrowsing->setTabText(5, "🚪 Exit App");
+
+    //ui->add_user_2->move()
+
+    ///=============web / pages titles END================
+
+    //testing for the New/final version:
+
+    //page USER
+    connect(ui->add_user  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(0);
+    });
+    connect(ui->Manage  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+    connect(ui->Display  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(2);
+    });
+    connect(ui->add_user_2  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(0);
+    });
+    connect(ui->Manage_2  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+    connect(ui->Display_2  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(2);
+    });
+    connect(ui->add_user_3  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(0);
+    });
+    connect(ui->Manage_3 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+    connect(ui->Display_3  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(2);
+    });
+
+    connect(ui->add_user_3  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(0);
+    });
+    connect(ui->Manage_3 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+    connect(ui->Display_3  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget->setCurrentIndex(2);
+    });
+    //page DOCK
+    connect(ui->add_user_4  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(0);
+    });
+    connect(ui->Manage_4 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(1);
+    });
+    connect(ui->Display_4  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(2);
+    });
+    connect(ui->add_user_5  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(0);
+    });
+    connect(ui->Manage_5 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(1);
+    });
+    connect(ui->Display_5  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(2);
+    });
+    connect(ui->add_user_6  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(0);
+    });
+    connect(ui->Manage_6 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(1);
+    });
+    connect(ui->Display_6  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_2->setCurrentIndex(2);
+    });
+
+    //page BOAT :,D
+    connect(ui->add_user_7  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(0);
+    });
+    connect(ui->Manage_7 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(1);
+    });
+    connect(ui->Display_7  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(2);
+    });
+    connect(ui->add_user_8  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(0);
+    });
+    connect(ui->Manage_8 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(1);
+    });
+    connect(ui->Display_8  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(2);
+    });
+    connect(ui->add_user_9  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(0);
+    });
+    connect(ui->Manage_9 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(1);
+    });
+    connect(ui->Display_9  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_3->setCurrentIndex(2);
+    });
+    //page Prod
+    connect(ui->add_user_10  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(0);
+    });
+    connect(ui->Manage_10 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(1);
+    });
+    connect(ui->Display_10  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(2);
+    });
+    connect(ui->add_user_11  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(0);
+    });
+    connect(ui->Manage_11 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(1);
+    });
+    connect(ui->Display_11  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(2);
+    });
+    connect(ui->add_user_12  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(0);
+    });
+    connect(ui->Manage_12 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(1);
+    });
+    connect(ui->Display_12  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_4->setCurrentIndex(2);
+    });
+    //cOMPANIES
+    connect(ui->add_user_13  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(0);
+    });
+    connect(ui->Manage_13 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(1);
+    });
+    connect(ui->Display_13  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(2);
+    });
+    connect(ui->add_user_14  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(0);
+    });
+    connect(ui->Manage_14 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(1);
+    });
+    connect(ui->Display_14  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(2);
+    });
+    connect(ui->add_user_15 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(0);
+    });
+    connect(ui->Manage_15 , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(1);
+    });
+    connect(ui->Display_15  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(2);
+    });
+    connect(ui->CheckInsightsComp  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(3);
+    });
+    connect(ui->GoBackCompanies  , &QPushButton::clicked, this, [=]() {
+        ui->stackedWidget_5->setCurrentIndex(2);
+    });
+    //++++
+
+    /// Exit page
+    connect(ui->Exit, &QPushButton::clicked, qApp, &QApplication::quit);
+
+    /// Log in page
+    connect(ui->Goback, &QPushButton::clicked, this, [=]() {
+        MainWindow *w = new MainWindow();
+        w->show();
+        this->close();   // or hide()
+    });
 }
 
 appwindow::~appwindow()

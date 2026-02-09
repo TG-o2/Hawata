48d52d0a36122510ad6840ea072a6cdd3b6eacda
Tasnim Guesmi <tasnimguesmi02@gmail.com>
2026-02-09 22:48:38 +0100

    Delete build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Sign_up_FILE C:/Users/BenKh/OneDrive/Desktop/PROJET C++ QT/Project_qt/Sign_up/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/Sign_up.exe)
-set(__QT_DEPLOY_TARGET_Sign_up_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Sign_up_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
